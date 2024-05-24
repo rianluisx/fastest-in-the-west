2e0cf70a590d0b593191f562f72b1c75e2ae48f0
rianluisx <rcmabait@up.edu.ph>
2024-05-24 21:34:34 +0800

    css + menu view

--- views/menu-view.php
+++ views/menu-view.php
@@ -2,8 +2,33 @@
 
     include '../DBConnector.php';
 
+    $getItems = "SELECT * from menu";
+    $result = $conn->query($getItems);
+    if ($result->num_rows > 0) {
+
+        while ($row = $result->fetch_assoc()) {
+
+            echo "<link rel='stylesheet' href='../css/cards.css'>";
+
+            echo "<div class ='menu-card'>";
+               
+            
+                echo "<img src = ' ". $row["food_image"] . "' alt = ' ". $row["food_name"] . "' >";
+                echo $row["food_name"];
+
+
+
+            echo "</div>";
+
+
+
+        }
+    } else {
+
+    }
+
+
     
-    echo 'test';
 
 
 
