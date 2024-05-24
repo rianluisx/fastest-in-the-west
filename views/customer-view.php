c15a35dacd277df261de025430bf7a3003878f55
rianluisx <rcmabait@up.edu.ph>
2024-05-25 07:38:40 +0800

    added inserts and followed flow

---
+++ views/customer-view.php
@@ -0,0 +1,28 @@
+<?php 
+
+    include "../DBConnector.php";
+
+    echo "<link rel='stylesheet' href='../css/cards.css'> ";
+    echo "<link rel='stylesheet' href='../css/style.css'>";
+    echo "<link rel='stylesheet' href='../css/buttons.css'>";
+
+    echo "<div class='cards-container-customer'>" .
+            "<div class='cards-customer'>" .
+                "<br><br><h3>Customer Order Form</h3><br><br>".
+                "<form action='menu-view.php' method='post'>" .
+                    "<label for ='customerName' > Name: </label>".
+                    "<input type='text' name='customer_name' id='customerName' required>".
+                    "<br><br>".
+                    "<label for ='customerAddress' > Address: </label>".
+                    "<input type='text' name='customer_address' id='customerAddress'>".
+                    "<br><br>".
+                    "<button type='submit' class='order'>Make an Order</button>" .
+                "</form>" .
+                "<div class='container-customer'>" .
+                "</div>" .
+            "</div>" .
+            "<br>" .
+        "</div>";
+
+
+?>
