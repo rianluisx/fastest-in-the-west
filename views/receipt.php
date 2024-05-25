c2abc1f8ff3a3adc855103fe69d45e5d3366f46b
rianluisx <rcmabait@up.edu.ph>
2024-05-25 23:03:55 +0800

    added receipts

--- views/receipt.php
+++ views/receipt.php
@@ -1,91 +1,80 @@
 <?php 
 
-    // this will be used for printing ang receipt progress palang
-
+    session_start();
     include '../DBConnector.php';
 
-    
-    if (isset($_POST['food_ID'])){
+    if (!isset($_SESSION['orders']) || empty($_SESSION['orders'])) {
+        echo "<script>alert('No orders placed yet'); window.location.href = 'menu-view.php';</script>";
+        exit();
+    }
 
-        $food_ID = $_POST["food_ID"];
-        $foodAttributes = "SELECT * FROM menu WHERE food_ID = $food_ID";
-        $retrievedAttributes = $conn->query($foodAttributes);
-        $customer_ID = $_POST["customer_ID"];
+    echo "<link rel='stylesheet' href='../css/cards.css'> ";
+    echo "<link rel='stylesheet' href='../css/style.css'>";
+    echo "<link rel='stylesheet' href='../css/table-view.css'>";
+    echo "<link rel='stylesheet' href='../css/buttons.css'>";
+
+    echo "<div class='receipt-container'>";
+        echo "<h1 style='text-align: center; padding-top: 10px;'>Order Receipt</h1>";
+        echo "<br>";
+
+        $totalAmount = 0;
+
+        echo "<table>";
     
-        if ($retrievedAttributes->num_rows > 0) {
+        echo "<tr>
+                <th>Food Item</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Total</th>
+            </tr>";
+
+
+        foreach ($_SESSION['orders'] as $food_ID => $quantity) {
             
-    
-            $row = $retrievedAttributes->fetch_assoc();
-    
-            echo "<link rel='stylesheet' href='../css/cards.css'> ";
-            echo "<link rel='stylesheet' href='../css/style.css'>";
-            echo "<link rel='stylesheet' href='../css/buttons.css'>";
+            $foodQuery = "SELECT food_name, price FROM menu WHERE food_ID = $food_ID";
+            $result = $conn->query($foodQuery);
 
-            echo "<div class='cards-container-food'>" .
-                "<div class='cards-food'>" .
- 
-                    "<img class='center' src='" . $row['food_image'] . "' alt='" . $row['food_name'] . "'>" .
- 
-                    "<div class='container-food'>" .
-                        "<h1><b>" . $row["food_name"] . "</b></h1>" .
-                        "<h3> $" . $row["price"] . "</h3>" .
-                        "<p class='lorem'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </p>".
+            if ($result->num_rows > 0) {
 
-                        "<div class='quantity center'>
-                            <form action = 'order-view.php' method = 'post'>
-                                <label for ='quantity'> Quantity: </label>
-                                <input type='hidden' name='food_ID' value='" . $row['food_ID'] . "'>
-                                <input type='hidden' name='food_name' value='" . $row['food_name'] . "'>
-                                <input type='hidden' name='price' value='" . $row['price'] . "'>
-                                <input type='hidden' name='customer_ID' value='" . $customer_ID . "'>
-                                <input type='number' id='quantity' name='quantity' value='1' min='1'>
-                                <button type='button' onclick='decrement()'>-</button>
-                                <button type='button' onclick='increment()'>+</button>
-                                <button type='submit' class='order' name='submit'> Submit Order </button>
-                            </form>
-                        </div>".
+                $row = $result->fetch_assoc();
+                $foodName = $row['food_name'];
+                $price = $row['price'];
+                $totalPrice = $price * $quantity;
+                $totalAmount += $totalPrice;
 
-    
-                    "</div>" .
-                    
-                "</div>" .
-                "<br>" .
-            "</div>";
-
-            echo "<script>
-                    function increment() {
-                        var input = document.getElementById('quantity');
-                        input.value = parseInt(input.value) + 1;
-                    }
-
-                    function decrement() {
-                        var input = document.getElementById('quantity');
-                        if (parseInt(input.value) > 1) {
-                            input.value = parseInt(input.value) - 1;
-                        }
-                    }
-                </script>";
-
-
-    
-
-    
-        } else {
-            echo "<script>
-                alert('No matching food item')
-            </script>";
+                echo "<tr>";
+                    echo "<td>" . $foodName . "</td>";
+                    echo "<td>" . $quantity . "</td>";
+                    echo "<td>$" . number_format($price, 2) . "</td>";
+                    echo "<td>$" . number_format($totalPrice, 2) . "</td>";
+                echo "</tr>";
+            }
         }
 
+        echo "<tr>";
+            echo "<td colspan='3' style='text-align:right;'><b>Total Amount</b></td>";
+            echo "<td><b>$" . number_format($totalAmount, 2) . "</b></td>";
+        echo "</tr>";
 
+        echo "</table>";
+        echo "<div style='text-align: center; margin-top: 20px;'>";
+        echo "<button onclick='goBack()' class='go-back'>Go Back</button>";
+        echo "</div>";
 
+    echo "</div>";
 
-    } else {
-        echo "<script>
-            alert('Food ID not passed properly!')
+    echo "<script>
+            function goBack() {
+                window.location.href = 'menu-view.php';
+            }
         </script>";
-    }
+
+    $conn->close();
+
+
+
+
+
 
 
 
