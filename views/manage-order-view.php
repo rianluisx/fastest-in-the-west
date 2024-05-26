862570caf09719e7e87e46cb6e1bd554c9026ce6
Andry <ajtajaja13@gmail.com>
2024-05-26 22:55:05 +0800

    changed date format for more readability

--- views/manage-order-view.php
+++ views/manage-order-view.php
@@ -19,7 +19,8 @@ if ($orderResult->num_rows > 0) {
         $customer = $conn->query($customerQuery)->fetch_assoc();
         $totalPriceQuery = "SELECT SUM(order_price) AS total_price FROM order_details WHERE order_ID = '".$row['order_ID']."';";
         $totalPrice = $conn->query($totalPriceQuery)->fetch_assoc()['total_price'];
-        
+        $order_date = (new DateTime($row['order_date']))->format("M d, Y h:i:s a");
+
         echo "<div class='cards-container'>" .
                 "<div class='cards'>".
                     "<div class='order-info'>";
@@ -35,7 +36,7 @@ if ($orderResult->num_rows > 0) {
                         }
 
                         echo "<p class='total'>Total: $". $totalPrice . "</p>" .
-                            "<p class='date'>Time: ". $row['order_date']. "</p>";
+                            "<p class='date'>Time: ". $order_date . "</p>";
             
                     echo "</div>" .
 
