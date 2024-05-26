8dc9a9fb238530bdda0784142e65889c6ee00536
rianluisx <rcmabait@up.edu.ph>
2024-05-26 21:55:50 +0800

    updated-some-css + layout of manage-menu

--- views/manage-order-view.php
+++ views/manage-order-view.php
@@ -1,57 +1,72 @@
 <?php 
+include '../DBConnector.php';
 
-    include '../DBConnector.php';
+$orderQuery = "SELECT * FROM orders";
+$orderResult = $conn->query($orderQuery);
 
-    
+echo "<link rel='stylesheet' href='../css/cards.css'> ";
+echo "<link rel='stylesheet' href='../css/style.css'>";
+echo "<link rel='stylesheet' href='../css/buttons.css'>";
+
+if ($orderResult->num_rows > 0) {
+    echo "<h2 class='inventory'>Ongoing orders</h2>" .
 
 
+    "<div class='cards-grid-container'>";
 
-    $orderQuery = "SELECT * FROM orders";
-    $orderResult = $conn->query($orderQuery);
-
-    if ($orderResult->num_rows > 0) {
-        echo "<link rel='stylesheet' href='../css/table-view.css'>";
-            echo "<h2>Inventory</h2><table><tr><th>Order ID</th><th>Customer Name</th><th>Customer Address</th><th>Total Price</th><th>Order Date</th><th>Action</th></tr>";
-
-            while ($row = $orderResult->fetch_assoc()) {
-                $customerQuery = "SELECT * FROM customer WHERE customer_ID = '".$row['customer_ID']."';";
-                $customer = $conn->query($customerQuery)->fetch_assoc();
-                $totalPriceQuery = "SELECT SUM(order_price) AS total_price FROM order_details WHERE order_ID = '".$row['order_ID']."';";
-                $totalPrice = $conn->query($totalPriceQuery)->fetch_assoc()['total_price'];
-                echo "<tr>
-                        <td>".$row['order_ID']."</td>
-                        <td>".$customer['customer_name']."</td>
-                        <td>".$customer['customer_address']."</td>
-                        <td>$".$totalPrice."</td>
-                        <td>".$row['order_date']."</td>
-                        <td>
-                            <form action='order-receipt.php' method='post'>
-                                <input type='hidden' name='order_ID' value=" . $row['order_ID'] . ">
-                                <button type='submit'>View Receipt</button>
-                            </form>
-                            <form action='../updates/send-out.php' method='post'>
-                                <input type='hidden' name='order_ID' value=" . $row['order_ID'] . ">
-                                <input type='hidden' name='customer_ID' value=" . $row['customer_ID'] . ">
-                                <button type='submit'>Send Out</button>
-                            </form>
-                        </td>
-                    </tr>";
-            }
-            echo "</table>";
-            echo "<div class='button-container'><form action='../src/index.php' method='post'>
-                        <button type='submit' class='wide-button'>Return</button>
-                </form></div>
-            ";
-    }else{
+    while ($row = $orderResult->fetch_assoc()) {
+        $customerQuery = "SELECT * FROM customer WHERE customer_ID = '".$row['customer_ID']."';";
+        $customer = $conn->query($customerQuery)->fetch_assoc();
+        $totalPriceQuery = "SELECT SUM(order_price) AS total_price FROM order_details WHERE order_ID = '".$row['order_ID']."';";
+        $totalPrice = $conn->query($totalPriceQuery)->fetch_assoc()['total_price'];
         
-        echo "<script>alert('No orders placed yet'); window.location.href = '../src/index.php';</script>";
-        exit();
+        echo "<div class='cards-container'>" .
+                "<div class='cards'>".
+                    "<div class='order-info'>";
+
+                        echo "<h1 class='orderID'>Order #" . $row['order_ID'] . "</h1>";
+                        echo "<div class='order-details'>";
+                        echo "<p class='customerName'>Customer: ". $customer['customer_name'] . "</p>";
+
+                        if (!empty($customer['customer_address'])) {
+                            echo "<p class='customerAdd'>Address: ". $customer['customer_address'] . "</p>";
+                        } else {
+                            echo "<p class='noAdd'> No address provided </p>";
+                        }
+
+                        echo "<p class='total'>Total: $". $totalPrice . "</p>" .
+                            "<p class='date'>Time: ". $row['order_date']. "</p>";
+            
+                    echo "</div>" .
+
+                    "<div class ='form-container'>".
+                        "<form action='order-receipt.php' method='post'>
+                            <input type='hidden' name='order_ID' value=" . $row['order_ID'] . ">
+                            <button type='submit'>View Receipt</button>
+                        </form>
+                        <form action='../updates/send-out.php' method='post'>
+                            <input type='hidden' name='order_ID' value=" . $row['order_ID'] . ">
+                            <input type='hidden' name='customer_ID' value=" . $row['customer_ID'] . ">
+                            <button type='submit'>Send Out</button>
+                        </form>" .
+                    "</div>".
+                "</div>".
+            "</div>".
+         "</div>";
+
     }
 
-    
-    
+    echo "</div>";
 
 
-
-
+    echo "<div class='button-container'>
+                <form action='../src/index.php' method='post'>
+                    <button type='submit' class='wide-button'>Return</button>
+                </form>
+            </div>
+    ";
+} else {
+    echo "<script>alert('No orders placed yet'); window.location.href = '../src/index.php';</script>";
+    exit();
+}
 ?>
--- views/menu-view.php
+++ views/menu-view.php
@@ -6,17 +6,17 @@
     if(isset($_SESSION['customer_name']) && isset($_SESSION['customer_address'])) {
         $menuQuery = "SELECT * FROM menu";
         $menuResult = $conn->query($menuQuery);
-        echo "<p class = 'menu-header'> Our Menu </p>";
+        
+        echo "<p class='menu-header'> Our Menu </p>";
 
         if ($menuResult->num_rows > 0) {
+            echo "<link rel='stylesheet' href='../css/cards.css'>";
+            echo "<link rel='stylesheet' href='../css/style.css'>";
+            echo "<link rel='stylesheet' href='../css/buttons.css'>";
+            
+            echo "<div class='cards-grid-container'>";
+            
             while ($row = $menuResult->fetch_assoc()) {
-                
-                echo "<link rel='stylesheet' href='../css/cards.css'> ";
-                echo "<link rel='stylesheet' href='../css/style.css'>";
-                echo "<link rel='stylesheet' href='../css/buttons.css'>";
-    
-
-
                 echo "<div class='cards-container'>" .
                         "<div class='cards'>" .
                             "<form action='food-view.php' method='post'>" .
@@ -27,13 +27,14 @@
                                 "<h4><b>" . $row["food_name"] . "</b></h4>" .
                             "</div>" .
                         "</div>" .
-                    "<br>" .
                     "</div>";
             }
-            echo "<br>";
+
+            echo "</div>";
+            
             echo "<div class='cstm-buttons'>" .
-                    "<button onclick ='registerOrder()' class='submit-order'> Submit Orders </button>" . "&nbsp" .
-                    "<button onclick ='viewOrders()' class='view-order'> View Orders </button>".
+                    "<button onclick='registerOrder()' class='submit-order'> Submit Orders </button>" . "&nbsp" .
+                    "<button onclick='viewOrders()' class='view-order'> View Orders </button>".
                 "</div>";
             
             echo "<script>
@@ -43,13 +44,13 @@
                     function viewOrders(){
                         window.location.href = '../views/customer-receipt.php';
                     }
-                    
                 </script>";
+
         } else {
             echo "<script>alert('No menu items available');</script>";
         }
     } else {
-        echo "<script>alert('An error occured with the session');</script>";
+            echo "<script>alert('An error occurred with the session');</script>";
     }
 
 ?>
